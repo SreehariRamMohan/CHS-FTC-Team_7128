72b81e08fa03085903809f42a8c07ae412371641
tnarayanan <tejasn100@gmail.com>
2016-10-01 14:32:18 -0700

    Removed @Disabled in Autonomous_Blue_v1 and Autonomous_Red_v1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Blue_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Blue_v1.java
@@ -68,7 +68,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  */
 
 @Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
-@Disabled
+//@Disabled
 public class Autonomous_Blue_v1 extends LinearOpMode {
 
     /* Declare OpMode members. */
