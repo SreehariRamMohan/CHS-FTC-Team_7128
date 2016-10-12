1bfdc4674b31925d2e107f1d18569c4a0979aa14
shruthij28 <shruthijaganathan@gmail.com>
2016-10-12 16:55:56 -0700

    Added a test for 180 degree servos using gamepad.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -3,6 +3,38 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by ShruthiJaganathan on 10/5/16.
  */
-public class ServoTest {
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoTest extends OpMode{
+
+    Servo servo;
+
+    @Override
+    public void init() {
+        servo = hardwareMap.servo.get("servo");
+    }
+
+    @Override
+    /*
+     > 0.5 is counterclockwise, 0.5 is full stop, < 0.5 is clockwise
+     Minimum is 0, Maximum is 1
+     */
+    public void loop() {
+
+        if(gamepad1.dpad_right){
+            servo.setPosition(0.2);
+        } else if(gamepad1.dpad_left){
+            servo.setPosition(0.7);
+        } else {
+            servo.setPosition(0.5);
+        }
+
+        telemetry.addData("Servo Position", servo.getPosition());
+    }
+
+    public void stop(){
+
+    }
 }
