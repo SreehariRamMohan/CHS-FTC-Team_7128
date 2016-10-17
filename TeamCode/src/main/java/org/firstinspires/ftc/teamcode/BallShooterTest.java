db36a562a1d538fb29fef33b171d21f2a6b60e3f
shruthij28 <shruthijaganathan@gmail.com>
2016-10-16 19:42:12 -0700

    Added a class to test the shooting mechanism with a gamepad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallShooterTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by ShruthiJaganathan on 10/16/16.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class BallShooterTest extends LinearOpMode{
+    Servo servo2 = hardwareMap.servo.get("servo_2");
+    DcMotor motor1 = hardwareMap.dcMotor.get("motor_1");
+    final double STOP_POSITION  = 0.5;
+    double currTime;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //bottom : a; right : b; left: x; top: y
+        if(gamepad1.y){
+            // < 0.5 is clockwise
+            servo2.setPosition(0.2);
+            //something needs to be added to the robot to stop the servo from moving the entire 180 degrees
+        }
+        servo2.setPosition(STOP_POSITION);
+
+        currTime = this.time;
+
+        while(this.time - currTime < 3) {
+            motor1.setPower(0.5);
+        }
+
+        motor1.setPower(0);
+    }
+
+    public void stopRobot(){
+        motor1.setPower(0);
+        servo2.setPosition(0.5);
+    }
+}
