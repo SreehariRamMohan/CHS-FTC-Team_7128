d5c8b72254fc0c9239cb9985c2abdab9a98bb54f
tnarayanan <tejasn100@gmail.com>
2016-10-19 16:49:41 -0700

    - Created AutonomousRedVuforia to enable Vuforia integration - Updated BallShooterTestAutonomous to use time instead of gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallShooterTestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallShooterTestAutonomous.java
@@ -1,9 +1,45 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 /**
- * Created by ShruthiJaganathan on 10/19/16.
+ * Created by ShruthiJaganathan on 10/16/16.
  */
-public class BallShooterTestAutonomous {
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
+public class BallShooterTestAutonomous extends LinearOpMode{
+    Servo servo2 = hardwareMap.servo.get("servo_2");
+    DcMotor motor1 = hardwareMap.dcMotor.get("motor_1");
+    final double STOP_POSITION  = 0.5;
+    double currTime = this.time;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //bottom : a; right : b; left: x; top: y
+
+        while (this.time - currTime < 1.5) {
+            // wait
+        }
+
+        servo2.setPosition(0.2);
+        //something needs to be added to the robot to stop the servo from moving the entire 180 degrees
+
+        servo2.setPosition(STOP_POSITION);
+
+        currTime = this.time;
+
+        while(this.time - currTime < 3) {
+            motor1.setPower(0.5);
+        }
+
+        motor1.setPower(0);
+    }
+
+    public void stopRobot(){
+        motor1.setPower(0);
+        servo2.setPosition(0.5);
+    }
 }
