d11f3979b2d7c76652fb41c4b58fa810fb1e4b71
shruthij28 <shruthijaganathan@gmail.com>
2016-10-22 15:42:54 -0700

    Finished Tele-Op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_v1.java
@@ -8,20 +8,78 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by ShruthiJaganathan on 10/19/16.
  */
 public class TeleOp_v1  extends OpMode{
-    Servo servo1 = hardwareMap.servo.get("servo_1"); // Beacon Press servo
-    Servo servo2 = hardwareMap.servo.get("servo_2"); // Ball Shooter servo
+    Servo servo1; // Beacon Press servo
+    Servo servo2; // Ball Shooter servo
 
-    DcMotor motor1 = hardwareMap.dcMotor.get("motor_1"); // Ball Shooter motor
-    DcMotor rightMotor = hardwareMap.dcMotor.get("right_m"); // Right side wheels
-    DcMotor leftMotor = hardwareMap.dcMotor.get("left_m"); // Left Side Wheels
-    DcMotor sweeper = hardwareMap.dcMotor.get("sweeper_m"); // Sweeper motor
+    DcMotor motor1; // Ball Shooter motor
+    DcMotor rightMotor; // Right side wheels
+    DcMotor leftMotor; // Left Side Wheels
+    DcMotor sweeper; // Sweeper motor
 
     @Override
     public void init() {
-
+        servo1 = hardwareMap.servo.get("servo_1");
+        servo2 = hardwareMap.servo.get("servo_2");
+        motor1 = hardwareMap.dcMotor.get("motor_1");
+        rightMotor = hardwareMap.dcMotor.get("right_m");
+        leftMotor = hardwareMap.dcMotor.get("left_m");
+        sweeper = hardwareMap.dcMotor.get("sweeper_m");
     }
 
     @Override
     public void loop() {
+        // Gamepad 1
+
+        if(gamepad1.right_stick_y == 1) {
+          rightMotor.setPower(1); //moves right wheels forward
+        } else if(gamepad1.right_stick_y == -1) {
+            rightMotor.setPower(-1); //moves right wheels backward
+        } else {
+            rightMotor.setPower(0); //stops right wheels
+        }
+
+        if(gamepad1.left_stick_y == 1){
+            leftMotor.setPower(1); //moves left wheels forward
+        } else if(gamepad2.left_stick_y == -1){
+            leftMotor.setPower(-1); //moves left wheels backward
+        } else {
+            leftMotor.setPower(0); //stops left wheels
+        }
+
+        if(gamepad1.y){
+            sweeper.setPower(1); //surgical tubing forward
+        } else if(gamepad1.a){
+            sweeper.setPower(-1); //surgical tubing backward
+        } else if(gamepad1.x){
+            sweeper.setPower(0); //stop surgical tubing
+        }
+
+        //gamepad 2
+
+        if(gamepad2.dpad_left){
+            servo1.setPosition(0.7); //beacon press left
+        } else if(gamepad2.dpad_right){
+            servo1.setPosition(0.3); //beacon press right
+        } else {
+            servo1.setPosition(0.5); //caution: stop
+        }
+
+        if(gamepad2.y){
+            servo2.setPosition(0.7); //ball shooter servo up
+        } else if(gamepad2.a){
+            servo2.setPosition(0.3); //ball shooter servo  down
+        } else if(gamepad2.x){
+            servo2.setPosition(0.5); //stop ball shooter servo
+        }
+
+        if(gamepad2.right_stick_y == 1){
+            motor1.setPower(1); //turn on ball shooting motor
+        } else if(gamepad2.right_stick_y == -1){
+            motor1.setPower(-1); //turn motor backwards
+        } else if(gamepad2.b){
+            motor1.setPower(0); //stop ball shooting motor
+        }
+
+
     }
 }
