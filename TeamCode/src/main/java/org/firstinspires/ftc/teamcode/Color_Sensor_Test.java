1993297b982fce7c7160a10292d7009e9d3c26f3
tnarayanan <tejasn100@gmail.com>
2016-11-05 14:12:19 -0700

    Updated Color Sensor Test program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Sensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Sensor_Test.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.LED;
@@ -14,56 +16,58 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
  * Created by robotics on 10/26/16.
  */
 
+@Autonomous(name="Color Sensor Test", group = "Autonomous")
 public class Color_Sensor_Test extends LinearOpMode {
 
-    boolean activateServo = true;
-    double servoPosition;
-    Servo servo1 = hardwareMap.servo.get("servo_1");
-    ColorSensor cr = hardwareMap.colorSensor.get("mr");
-    public static final double MAX_POSITION = 1;
-    public static final double MIN_POSITION = 0;
-    double currentTime;
+    ColorSensor cr;
     LED led;
-    TouchSensor t;
+
     /*
     servo1 = hardwareMap.servo.get("servo_1");
     cr = hardwareMap.colorSensor.get("mr");
     */
-   public void runOpMode() {
+    public void runOpMode() {
 
-       led = hardwareMap.led.get("led");
-       t = hardwareMap.touchSensor.get("t");
+        try {
+            cr = hardwareMap.colorSensor.get("mr");
+            led = hardwareMap.led.get("led");
 
-       // wait for start();
+            // wait for start();
 
-       float hsvValues[] =  {0, 0, 0};
-       final float values[] = hsvValues;
-       final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
+            float hsvValues[] = {0, 0, 0};
+            final float values[] = hsvValues;
+            final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
 
-       enabledLed(t.isPressed());
 
-       Color.RGBToHSV(cr.red()*8, cr.green()*8, cr.blue() * 8, hsvValues);
+            Color.RGBToHSV(cr.red() * 8, cr.green() * 8, cr.blue() * 8, hsvValues);
 
-       double redV = cr.red();
-       double blueV = cr.blue();
+            double redV = cr.red();
+            double blueV = cr.blue();
 
-       telemetry.addData("Clear", cr.alpha());
-       telemetry.addData("Red  ", cr.red());
-       telemetry.addData("Green", cr.green());
-       telemetry.addData("Blue ", cr.blue());
-       telemetry.addData("Hue", hsvValues[0]);
-       double time = this.time;
-       while (this.time - time < 30) {
-           if(blueV-redV >= 50) {
-               telemetry.addData("This is blue!" , cr.blue());
-           } else if (redV - blueV >= 50){
-               telemetry.addData("This is red" , cr.red());
-           }
-       }
+            telemetry.addData("Clear", cr.alpha());
+            telemetry.addData("Red  ", cr.red());
+            telemetry.addData("Green", cr.green());
+            telemetry.addData("Blue ", cr.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+            double time = this.time;
+            while (this.time - time < 30) {
 
-   }
+                // Must update redV and blueV here
 
-    private void enabledLed(boolean value) {cr.enableLed(value);}
+                if (blueV - redV >= 50) {
+                    telemetry.addData("This is blue!", cr.blue());
+                } else if (redV - blueV >= 50) {
+                    telemetry.addData("This is red", cr.red());
+                }
+            }
+        }
+        catch(NullPointerException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
 }
 
