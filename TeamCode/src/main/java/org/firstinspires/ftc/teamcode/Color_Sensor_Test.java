24dff39411b23f5bb186bb9c3a0c4ceccd3c5a1e
tnarayanan <tejasn100@gmail.com>
2016-11-09 15:47:59 -0800

    Updated contents of loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Sensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Sensor_Test.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -11,6 +13,8 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.LED;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import static android.widget.Toast.*;
 
 /**
  * Created by robotics on 10/26/16.
@@ -20,55 +24,54 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 public class Color_Sensor_Test extends LinearOpMode {
 
     ColorSensor cr;
-    LED led;
+    //LED led;
 
     /*
     servo1 = hardwareMap.servo.get("servo_1");
     cr = hardwareMap.colorSensor.get("mr");
     */
+
+
+
     public void runOpMode() {
 
-        try {
-            cr = hardwareMap.colorSensor.get("mr");
-            led = hardwareMap.led.get("led");
+        telemetry.addData("Test", "Test");
 
-            // wait for start();
+        cr = hardwareMap.colorSensor.get("mr");
+        //led = hardwareMap.led.get("led");
 
-            float hsvValues[] = {0, 0, 0};
-            final float values[] = hsvValues;
-            final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
+        // wait for start();
+
+        float hsvValues[] = {0, 0, 0};
+        final float values[] = hsvValues;
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
 
 
-            Color.RGBToHSV(cr.red() * 8, cr.green() * 8, cr.blue() * 8, hsvValues);
+        Color.RGBToHSV(cr.red() * 8, cr.green() * 8, cr.blue() * 8, hsvValues);
 
-            double redV = cr.red();
-            double blueV = cr.blue();
+        double redV;
+        double blueV;
+
+
+        double time = this.time;
+        while (this.time - time < 30) {
 
             telemetry.addData("Clear", cr.alpha());
             telemetry.addData("Red  ", cr.red());
             telemetry.addData("Green", cr.green());
             telemetry.addData("Blue ", cr.blue());
             telemetry.addData("Hue", hsvValues[0]);
-            double time = this.time;
-            while (this.time - time < 30) {
 
-                // Must update redV and blueV here
+            redV = cr.red();
+            blueV = cr.blue();
 
-                if (blueV - redV >= 50) {
-                    telemetry.addData("This is blue!", cr.blue());
-                } else if (redV - blueV >= 50) {
-                    telemetry.addData("This is red", cr.red());
-                }
+            if (blueV - redV >= 50) {
+                telemetry.addData("This is blue!", cr.blue());
+            } else if (redV - blueV >= 50) {
+                telemetry.addData("This is red", cr.red());
             }
         }
-        catch(NullPointerException e) {
-            e.printStackTrace();
-        }
-
-
     }
-
-
 }
 
 
